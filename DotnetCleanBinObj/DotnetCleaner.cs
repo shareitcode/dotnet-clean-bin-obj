c5c1780a139929e5a39039c57059bc3431a909cc
Share IT Code <45316299+shareitcode@users.noreply.github.com>
2022-11-25 21:13:32 +0100

    Repo initialization

---
+++ DotnetCleanBinObj/DotnetCleaner.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace DotnetCleanBinObj
+{
+    internal static class DotnetCleaner
+    {
+        public static void CleanBinObjFolders()
+        {
+            var processStart = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                Arguments = "Get-ChildItem .\\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }",
+                CreateNoWindow = true
+            };
+            Process.Start(processStart);
+            Console.WriteLine("Folders cleaned!");
+        }
+    }
+}
---
+++ DotnetCleanBinObj/Program.cs
@@ -0,0 +1,6 @@
+﻿using DotnetCleanBinObj;
+
+Console.WriteLine("***** Clean dotnet bin/obj folders - START *****");
+DotnetCleaner.CleanBinObjFolders();
+Console.WriteLine("***** Clean dotnet bin/obj folders - END *****");
+Console.ReadKey();
